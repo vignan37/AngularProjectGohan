7ef3ebb28b0d4e7945976ffb0145612ee7049661
Vignan Kurme <vignan37@gmail.com>
2023-05-10 07:23:35 +0000

    first commit

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-navbar',
+    template: '<h1>Navbar Component</h1>',
+    styles:[`h1{color: red;
+                font-size:50px;}`]
+})
+
+export class NavbarComponent{
+
+}
