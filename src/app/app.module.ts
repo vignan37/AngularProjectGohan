01a3f1d2508a3578b94851f7bd01c38a82c5911c
Vignan Kurme <vignan37@gmail.com>
2023-05-10 09:24:49 +0000

    post comp added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,9 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-navbar',
-    template: '<h1>Navbar Component</h1>',
-    styles:[`h1{color: red;
-                font-size:50px;}`]
+    templateUrl: './navbar.component.html',
+    styleUrls:[`./navbar.component.css`]
 })
 
 export class NavbarComponent{
