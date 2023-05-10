e9e46079f73307fec0e18d7dd709e56a3eff9d87
Vignan Kurme <vignan37@gmail.com>
2023-05-10 09:45:29 +0000

    Variable exchange from parent to child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ProjectGohan';
+  parentMessage:string='Message from the parent to child post component';
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class PostComponent {
   title:string = "List of Posts";
+  
+  @Input() fromParent!:string;
 }
